e47d95392362b03157ead63ec178a064678c7efa
2344webber <52434733+2344webber@users.noreply.github.com>
2019-10-31 12:16:45 +0800

    Add files via upload

--- Project/cmds/meme.py
+++ Project/cmds/meme.py
@@ -22,9 +22,10 @@ class meme(commands.Cog):
     
     @commands.command()
     async def commands(self,ctx):
-        embed=discord.Embed(title="本機器人指令", color=0x51a6c6)
+        embed=discord.Embed(title="梗圖指令", color=0x51a6c6)
         embed.add_field(name="`HI!warmeme`", value="會隨機跑出戰爭迷因(有彩蛋)", inline=False)
         embed.add_field(name="`HI!deaf`", value="會隨機跑出嗆人圖片", inline=True)
+        embed.add_field(name="其他指令",vaule="```製作中~~```")
         await ctx.send(embed=embed)
 
 
