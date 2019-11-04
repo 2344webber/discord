e83cb3cfa942b9c2f2e21871d4abc8bd4f7ef926
2344webber <52434733+2344webber@users.noreply.github.com>
2019-11-04 12:07:47 +0800

    Update meme.py

--- Project/cmds/meme.py
+++ Project/cmds/meme.py
@@ -19,15 +19,6 @@ class meme(commands.Cog):
     async def deaf(self,ctx):
         random_pic=random.choice(jdata['deaf'])
         await ctx.send(random_pic)
-    
-    @commands.command()
-    async def commands(self,ctx):
-        embed=discord.Embed(title="梗圖指令", color=0x51a6c6)
-        embed.add_field(name="`HI!warmeme`", value="會隨機跑出戰爭迷因(有彩蛋)", inline=False)
-        embed.add_field(name="`HI!deaf`", value="會隨機跑出嗆人圖片", inline=True)
-        embed.add_field(name="其他指令",vaule="```製作中~~```")
-        await ctx.send(embed=embed)
-
 
 def setup(bot):
     bot.add_cog(meme(bot))
